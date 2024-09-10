621efc4142ad8f8fa7cccde74e4f1280abb1d53d
Iker Martinez <ikermtz16@outlook.com>
2024-09-09 19:12:41 -0600

    Se realiza validaciones - v.3.4

--- OTP.js
+++ OTP.js
@@ -4,21 +4,29 @@ let OTPGenerated = '';
 let OTPEntered = '';
 
 const Status = Object.freeze({
-    VALID: 'OTP Valid',
-    INVALID: 'OTP Invalid',
-    CANCEL: 'Process Cancel',
-    OTHER: 'Another Process'
+    OTP_VALID: 'OTP OTP_VALID',
+    OTP_INVALID: 'OTP OTP_INVALID',
+    PROCESS_CANCEL: 'Process PROCESS_CANCEL',
+    ANOTHER: 'Another Process'
+});
+
+const Message = Object.freeze({
+    NO_TYPED: 'The OTP wasnt typed',
+    OTP_VALID: 'The OTP is Valid',
+    OTP_INVALID: 'The OTP is Invalid',
+    PROCESS_CANCEL: 'Process Cancelled',
+    PROCESS_STOPPED: 'Process Stopped'
 });
 
 function startInterval() {
 
     if (intervalId) {
-        console.log(intervalId);
         stopInterval();
     }
 
     OTPGenerated = generateOTP();
     if (OTPGenerated) {
+        console.log(OTPGenerated);
         /*console.log('hola');
         const copiarContenido = async () => {
             try {
@@ -39,34 +47,56 @@ function startInterval() {
         seconds--;
 
         if (seconds <= 0) {
-            validateOTP();
+            console.log(Message.NO_TYPED);
             stopInterval();
-            startInterval();
+            //startInterval();
         }
 
     }, 1000);
 }
 
-function stopInterval() {
+function stopInterval(status) {
+    if (status == Status.OTP_VALID) {
+        console.log(`El OTP Generated is ${OTPGenerated}, and the OTP Entered is ${OTPEntered}`);
+        console.log(Message.OTP_VALID);
+    } else if (status == Status.OTP_INVALID) {
+        console.log(`El OTP Generated is ${OTPGenerated}, and you type this ${OTPEntered}`);
+        console.log(Message.OTP_INVALID);
+    } else if (status == Status.PROCESS_CANCEL) {
+        console.log(Message.PROCESS_CANCEL);
+    } else if (status == Status.ANOTHER) {
+        console.log(Message.PROCESS_STOPPED);
+    }
+
+    console.log(intervalId);
     clearInterval(intervalId);
+    clearInterval(intervalId);
+    console.log(intervalId);
+    OTPGenerated = '';
     seconds = 20;
     sSec = '';
     console.log('');
     console.log('The process is finished.');
 }
 
-function validateOTP() {
-    if (OTPGenerated == OTPGenerated) {
-        console.log('');
-        console.log(`El OTP Generated is ${OTPGenerated}, and the OTP Entered is ${OTPEntered}`)
-        console.log('OTP Validated.');
-        stopInterval();
-    } else {
-        console.log('OTP not Validated');
-        stopInterval();
-        startInterval();
+function validateOTP(status) {
+    if (status == Status.TYPED) {
+        if (OTPGenerated == OTPEntered) {
+            stopInterval(Status.OTP_VALID);
+        } else if (OTPGenerated !== OTPEntered) {
+            stopInterval(Status.OTP_INVALID);
+            startInterval();
+        } else {
+            stopInterval(Status.ANOTHER);
+            startInterval();
+        }
+    } else if (status === Status.PROCESS_CANCEL) {
+        stopInterval(Status.PROCESS_CANCEL);
+    } else if (status == Status.ANOTHER) {
+        stopInterval(Status.ANOTHER);
     }
 }
+
 
 function generateOTP() {
     /*
@@ -80,20 +110,23 @@ function generateOTP() {
       */
 
     //Generate a random numeric with 6 digits
-    return Math.floor(100000 + Math.random() * 900000);
+    //return Math.floor(100000 + Math.random() * 900000);
+    let digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 6; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
 }
 
 startInterval();
 
 if (OTPGenerated != null) {
     if (OTPEntered !== null) {
-        console.log(`The OTP typed is ${OTPEntered}`);
-        validateOTP();
+        validateOTP(Status.TYPED);
     } else if (OTPEntered === null) {
-        console.log('Proceso Cancelado');
-        stopInterval();
+        validateOTP(Status.PROCESS_CANCEL);
     } else {
-        console.log('This OTP is incorrect');
-        stopInterval();
+        validateOTP(Status.ANOTHER);
     }
 }
